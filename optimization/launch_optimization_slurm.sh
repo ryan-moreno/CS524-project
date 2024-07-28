95db38519ec4fc3c7686c9d79ae10553a6754924
ryan-moreno <rrmoreno@wisc.edu>
2024-07-28 16:59:20 -0500

    Troubleshoot launching brute force model on slurm

--- optimization/launch_optimization_slurm.sh
+++ optimization/launch_optimization_slurm.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+#SBATCH --j=Optimize%j
+#SBATCH --ntasks=1
+#SBATCH --time=1:00:00
+#SBATCH --mem=16gb
+#SBATCH --error=/home/r/rrmoreno/524-proj/slurm-logs/Optimize%j.err
+#SBATCH --output=/home/r/rrmoreno/524-proj/slurm-logs/Optimize%j.out
+
 # Launch via:
-# sbatch \
-#     --job-name=optimize \
-#     --output=/home/r/rrmoreno/524-proj/slurm-logs/Optimize%j.log \
-#     --error=/home/r/rrmoreno/524-proj/slurm-logs/Optimize%j.err \
-#     launch_optimization_slurm.sh
+# sbatch launch_optimization_slurm.sh
 
 python optimization_model.py
--- optimization/optimization_model.py
+++ optimization/optimization_model.py
@@ -8,39 +8,44 @@
 
 import time
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
 # ## Load precomputed combinations to save computation time
 
-# In[8]:
+# In[2]:
 
 
-comb_df = pd.read_pickle("precompute_combinations/precomputed_combinations_df.pkl")
+comb_df = pd.read_pickle("../precompute_combinations/precomputed_combinations_df.pkl")
+
 # Set indices for faster accession
-comb_df.set_index(["n", "k"], inplace=True)
+comb_df["new_index"] = comb_df["n"] * 1201 + comb_df["k"]
+comb_df.set_index("new_index", inplace=True)
 comb_df.sort_index(inplace=True)
 
+
 def choose(n, k):
-    return comb_df.loc[(n, k), "n_choose_k"]
+    index = n * 1201 + k
+    return comb_df.loc[index, "n_choose_k"]
 
 
-# ## Load probabilities
-# 
-# TODO: right now this just hard codes some values
+# ## Probabilities based on our data analysis
 
 # In[3]:
 
 
 # Approximate probability of a goal in a given second
 seconds_in_game = 20 * 60 * 3
-p_a1 = 3.12 / seconds_in_game
+
+# Probability of a goal in a given second during 5v5
+p_a1 = 5.18 * 10**-4
 p_b1 = p_a1
 
 # Probability of the other team scoring during 6v5 increases
-p_b2 = 1.5 * p_b1
+p_b2 = 7.85 * 10**-4
 
 # Probability of our team scoring during 6v5 also increases, though not as much
-p_a1 = 1.25 * p_a1
+p_a1 = 7.22 * 10**-4
 
 
 # ## Inputs to model
@@ -57,39 +62,48 @@ T = 5 * 60
 
 
 # ## Brute force every choice of $s$
+#
+# This runs out of memory if run on my local computer. Laumching via slurm on SCC instead.
 
-# In[9]:
+# In[7]:
 
 
-decisions_df = pd.DataFrame(columns = [
-    'pull_time',
-    'prob_success',
-    'compute_time'])
+decisions_df = pd.DataFrame(columns=["pull_time", "prob_success", "compute_time"])
 
 #  Try out each possible time to pull the goalie
-for i in range(0, T):
-    s = i
-    if s % 30 == 0:
-        display(f"...computing for s={s} seconds")
+for s in range(1, T):
+
+    # Timing for performance metrics
     start_time = time.time()
 
     prob_success = 0
 
+    num_arrangements = 0
+
     # Number of goals scored by other team before pulling the goalie
     for g_b1 in range(0, s):
-        prob_gb1 = choose(s, g_b1) * (p_b1 ** g_b1) * ((1 - p_b1) ** (s - g_b1))
+        prob_gb1 = choose(s, g_b1) * (p_b1**g_b1) * ((1 - p_b1) ** (s - g_b1))
 
         # Number of goals scored by our team before pulling the goalie
         for g_a1 in range(0, s):
-            prob_ga1 = choose(s, g_a1) * (p_a1 ** g_a1) * ((1 - p_a1) ** (s - g_a1))
+            prob_ga1 = choose(s, g_a1) * (p_a1**g_a1) * ((1 - p_a1) ** (s - g_a1))
 
             # Number of goals scored by other team after pulling the goalie
             for g_b2 in range(0, T - s):
-                prob_gb2 = choose(T - s, g_b2) * (p_b2 ** g_b2) * ((1 - p_b2) ** (T - s - g_b2))
+                prob_gb2 = (
+                    choose(T - s, g_b2) * (p_b2**g_b2) * ((1 - p_b2) ** (T - s - g_b2))
+                )
 
                 # Number of goals scored by our team after pulling the goalie
                 for g_a2 in range(0, T - s):
-                    prob_ga2 = choose(T - s, g_a2) * (p_a1 ** g_a2) * ((1 - p_a1) ** (T - s - g_a2))
+                    num_arrangements += 1
+                    # display(f"Current arrangement: g_b1={g_b1}, g_a1={g_a1}, g_b2={g_b2}, g_a2={g_a2}")
+
+                    prob_ga2 = (
+                        choose(T - s, g_a2)
+                        * (p_a1**g_a2)
+                        * ((1 - p_a1) ** (T - s - g_a2))
+                    )
 
                     # Probability of this event
                     prob = prob_gb1 * prob_ga1 * prob_gb2 * prob_ga2
@@ -99,24 +113,36 @@ for i in range(0, T):
                     b = b_0 + g_b1 + g_b2
 
                     # Success if we win or tie
-                    success = (a >= b)
+                    success = a >= b
 
                     prob_success = prob_success + prob * success
+
+    prob_success = round(prob_success * 100, 4)
+
     end_time = time.time()
     elapsed_time = end_time - start_time
+    elapsed_time = round(elapsed_time, 2)
+
+    print(
+        f"Pull time: {s}, num_arrangements: {num_arrangements} Probability of success: {prob_success}, Compute time: {elapsed_time}"
+    )
 
     decisions_df = pd.concat(
         [
             decisions_df,
-            pd.DataFrame({
-                "pull_time": s,
-                "prob_success": prob_success,
-                "compute_time": elapsed_time
-            }, index=[0]),
-        ], ignore_index=True
+            pd.DataFrame(
+                {
+                    "pull_time": s,
+                    "prob_success": prob_success,
+                    "compute_time": elapsed_time,
+                },
+                index=[0],
+            ),
+        ],
+        ignore_index=True,
     )
 
-display(decisions_df)
+print(decisions_df)
 
 
 # ## Select the optimal choice
@@ -128,5 +154,9 @@ optimal_index = decisions_df["prob_success"].idxmax()
 optimal_time = decisions_df.loc[optimal_index, "pull_time"]
 optimal_prob = decisions_df.loc[optimal_index, "prob_success"]
 
-print(f"Optimal time to pull the goalie: {optimal_time} seconds, resulting in a probability of {optimal_prob} of winning or tying the game.")
+print(
+    f"Optimal time to pull the goalie: {optimal_time} seconds, resulting in a probability of {optimal_prob} of winning or tying the game."
+)
 
+# Plot pull time vs probability of success
+plt.plot(decisions_df["pull_time"], decisions_df["prob_success"])
