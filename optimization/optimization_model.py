56ef1ffa5622a8e794ad702e7651363de3912b09
ryan-moreno <rrmoreno@wisc.edu>
2024-07-31 17:07:36 -0500

    Format indices for precomputed combinations for speedier access

--- optimization/optimization_model.py
+++ optimization/optimization_model.py
@@ -3,7 +3,7 @@
 
 # To run via slurm, first output the ipynb notebook to python using `jupyter nbconvert --to script optimization_model.ipynb`
 
-# In[1]:
+# In[27]:
 
 
 import time
@@ -13,25 +13,20 @@ import matplotlib.pyplot as plt
 
 # ## Load precomputed combinations to save computation time
 
-# In[2]:
+# In[32]:
 
 
 comb_df = pd.read_pickle("../precompute_combinations/precomputed_combinations_df.pkl")
 
-# Set indices for faster accession
-comb_df["new_index"] = comb_df["n"] * 1201 + comb_df["k"]
-comb_df.set_index("new_index", inplace=True)
-comb_df.sort_index(inplace=True)
-
 
 def choose(n, k):
     index = n * 1201 + k
-    return comb_df.loc[index, "n_choose_k"]
+    return comb_df.at[index, "n_choose_k"]
 
 
 # ## Probabilities based on our data analysis
 
-# In[3]:
+# In[29]:
 
 
 # Approximate probability of a goal in a given second
@@ -45,12 +40,12 @@ p_b1 = p_a1
 p_b2 = 7.85 * 10**-4
 
 # Probability of our team scoring during 6v5 also increases, though not as much
-p_a1 = 7.22 * 10**-4
+p_a2 = 7.22 * 10**-4
 
 
 # ## Inputs to model
 
-# In[4]:
+# In[30]:
 
 
 # Let's say the current score is 1 - 3
@@ -63,12 +58,14 @@ T = 5 * 60
 
 # ## Brute force every choice of $s$
 #
-# This runs out of memory if run on my local computer. Laumching via slurm on SCC instead.
+# Launching via slurm on SSCC clusters to speed up
 
-# In[7]:
+# In[ ]:
 
 
 decisions_df = pd.DataFrame(columns=["pull_time", "prob_success", "compute_time"])
+
+results = []
 
 #  Try out each possible time to pull the goalie
 for s in range(1, T):
@@ -78,6 +75,7 @@ for s in range(1, T):
 
     prob_success = 0
 
+    # Tracking num_arrangements for complexity considerations
     num_arrangements = 0
 
     # Number of goals scored by other team before pulling the goalie
@@ -97,12 +95,10 @@ for s in range(1, T):
                 # Number of goals scored by our team after pulling the goalie
                 for g_a2 in range(0, T - s):
                     num_arrangements += 1
-                    # display(f"Current arrangement: g_b1={g_b1}, g_a1={g_a1}, g_b2={g_b2}, g_a2={g_a2}")
-
                     prob_ga2 = (
                         choose(T - s, g_a2)
-                        * (p_a1**g_a2)
-                        * ((1 - p_a1) ** (T - s - g_a2))
+                        * (p_a2**g_a2)
+                        * ((1 - p_a2) ** (T - s - g_a2))
                     )
 
                     # Probability of this event
@@ -124,25 +120,17 @@ for s in range(1, T):
     elapsed_time = round(elapsed_time, 2)
 
     print(
-        f"Pull time: {s}, num_arrangements: {num_arrangements} Probability of success: {prob_success}, Compute time: {elapsed_time}"
+        f"Pull time: {s}, num_arrangements: {num_arrangements} Probability of success: {prob_success}, Compute time: {elapsed_time}",
+        flush=True,
     )
 
-    decisions_df = pd.concat(
-        [
-            decisions_df,
-            pd.DataFrame(
-                {
-                    "pull_time": s,
-                    "prob_success": prob_success,
-                    "compute_time": elapsed_time,
-                },
-                index=[0],
-            ),
-        ],
-        ignore_index=True,
+    results.append(
+        {"pull_time": s, "prob_success": prob_success, "compute_time": elapsed_time}
     )
 
-print(decisions_df)
+decisions_df = pd.DataFrame(results)
+
+print(decisions_df, flush=True)
 
 
 # ## Select the optimal choice
@@ -155,7 +143,8 @@ optimal_time = decisions_df.loc[optimal_index, "pull_time"]
 optimal_prob = decisions_df.loc[optimal_index, "prob_success"]
 
 print(
-    f"Optimal time to pull the goalie: {optimal_time} seconds, resulting in a probability of {optimal_prob} of winning or tying the game."
+    f"Optimal time to pull the goalie: {optimal_time} seconds, resulting in a probability of {optimal_prob} of winning or tying the game.",
+    flush=True,
 )
 
 # Plot pull time vs probability of success
